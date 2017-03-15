3cfe8e0fcdfcb1c18a753405f0157f5a74196f56
Michael Leu <mleu@users.noreply.github.com>
2017-03-15 16:26:25 +0100

    add snapshot test for settings
    remove example tests from react-native init

--- __tests__/Settings.js
+++ __tests__/Settings.js
@@ -1,12 +1,13 @@
 import 'react-native';
 import React from 'react';
-import Index from '../index.android.js';
+import Settings from '../src/Settings.js';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-it.skip('renders correctly', () => {
+it('renders correctly', () => {
   const tree = renderer.create(
-    <Index />
-  );
+    <Settings />
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
 });
